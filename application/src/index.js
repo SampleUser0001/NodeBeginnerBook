0ecae28c96f1b1aa9cc9496a0dd37af345b9d5d0
Satoru Tanaka <ittimfn@gmail.com>
2020-12-20 10:46:01 +0900

    ルーティング

--- application/src/index.js
+++ application/src/index.js
@@ -1,3 +1,10 @@
-var server = require("./server");
+let server = require("./server");
+let router = require("./router");
+let requestHandlers = require("./requestHandlers");
 
-server.start();
+let handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle);
